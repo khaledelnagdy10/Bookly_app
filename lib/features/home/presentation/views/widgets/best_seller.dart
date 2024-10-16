ea5522a0d6fb29f2c759019737ed6578a0ecf207
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-10-16 22:35:07 +0300

    add Rating book

--- lib/features/home/presentation/views/widgets/best_seller.dart
+++ lib/features/home/presentation/views/widgets/best_seller.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSeller extends StatelessWidget {
   const BestSeller({super.key});
@@ -32,42 +33,62 @@ class BestSeller extends StatelessWidget {
               const SizedBox(
                 width: 30,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .5,
-                    child: const Text(
-                      'The Jungle Book',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontFamily: kGTSectraFineRegular),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Text('J.K.Rowling',
-                      style: Styles.textStyle14.copyWith(color: Colors.grey)),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        r'19.99$',
-                        style: Styles.textStyle20
-                            .copyWith(fontWeight: FontWeight.bold),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .5,
+                      child: const Text(
+                        'The Jungle Book',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontFamily: kGTSectraFineRegular),
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text('J.K.Rowling',
+                        style: Styles.textStyle14.copyWith(color: Colors.grey)),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          r'19.99$',
+                          style: Styles.textStyle20
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        RatingBook()
+                      ],
+                    )
+                  ],
+                ),
               )
             ],
           ),
         ),
       );
     });
+  }
+
+  Row RatingBook() {
+    return Row(
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.solidStar,
+              color: Colors.yellow,
+              size: 16,
+            )),
+        const Text('4.8'),
+        const Text('(265)'),
+      ],
+    );
   }
 }
