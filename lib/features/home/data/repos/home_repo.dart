1d29d99c1c2e33fa87b1993c8a6304cccfd6cafc
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-10-19 19:10:58 +0300

    create home repo methods & faliure file

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/utils/errors/faliure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Faliure, List<BookModel>>> fetchCoverBook();
+  Future<Either<Faliure, List<BookModel>>> fetchBestSellerBook();
+}
