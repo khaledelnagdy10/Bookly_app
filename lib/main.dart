c9d0642d31f5c4f1f8151c6ba3f7b91e33d2784b
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-10-21 16:17:02 +0300

    fskdjfkls

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/routers.dart';
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
